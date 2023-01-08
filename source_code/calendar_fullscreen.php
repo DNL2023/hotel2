9c346d281b39bc4a740f8f0caa0231e0de6ace22
Daniely2023 <122094790+Daniely2023@users.noreply.github.com>
2023-01-08 17:22:43 +0100

    full screen calendar

---
+++ source_code/calendar_fullscreen.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <script src='fullcalendar/dist/index.global.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@6.0.2/index.global.min.js" integrity="sha256-pp6WZj4ir6am4lRlG+1USgwGiXVAFSFmiKgRJegDHFM=" crossorigin="anonymous"></script>
+    <script>
+
+document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+            locale: 'en',
+            timeZone: 'UTC',
+            initialView: 'resourceTimelineDay',
+            headerToolbar: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth'
+              },
+            editable: true,
+            resourceAreaHeaderContent: 'Rooms',
+            resources: 'rest_api.php',
+            events: 'rest_api.php'
+        });
+
+        calendar.render();
+      });
+
+    </script>
+  </head>
+  <body>
+    <div id='calendar'></div>
+  </body>
+</html>
